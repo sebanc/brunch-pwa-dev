2108d0a43a7c4dd6eceee66cb3d2ea5a6ff5081f
sebanc <sebanc.xda@gmail.com>
2021-04-18 08:25:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -32,3 +32,19 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+
+self.addEventListener('notificationclick', event => {
+    const rootUrl = new URL('/', location).href;
+    event.notification.close();
+    // Enumerate windows, and call window.focus(), or open a new one.
+    event.waitUntil(
+      clients.matchAll().then(matchedClients => {
+        for (let client of matchedClients) {
+          if (client.url === rootUrl) {
+            return client.focus();
+          }
+        }
+        return clients.openWindow("/");
+      })
+    );
+});
