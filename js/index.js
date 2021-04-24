6a8b188c8f5b385f845f443a48429be7be642f87
sebanc <sebanc.xda@gmail.com>
2021-04-24 08:26:43 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ async function periodicsync() {
 navigator.serviceWorker.ready.then(registration => {
   registration.periodicSync.register('get-latest-news', {
     // Minimum interval at which the sync may fire.
-    minInterval: 12 * 60 * 60 * 1000,
+    minInterval: 15 * 60 * 1000,
   });
 });
 }
