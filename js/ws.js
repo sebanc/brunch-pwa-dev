8433ce19c114c36aab6dc3b9c8cd33b7727dfec2
sebanc <sebanc.xda@gmail.com>
2021-05-09 17:21:34 +0200

    Update ws.js

--- js/ws.js
+++ js/ws.js
@@ -44,7 +44,7 @@ function ws_connect() {
 			}
 			if (messages[0] === "latest-stable") {
 				if (notifications.value === "yes" && brunch_stable.value === "yes") {
-					if (latest_stable && latest_stable !== "" && messages[1] !== "" && latest_stable.value !== messages[1]) {
+					if (latest_stable && latest_stable.value !== "" && messages[1] !== "" && latest_stable.value !== messages[1]) {
 						showNotification("New brunch stable release available: " + messages[1], "brunch");
 					}
 				}
@@ -53,7 +53,7 @@ function ws_connect() {
 			}
 			if (messages[0] === "latest-unstable") {
 				if (notifications.value === "yes" && brunch_unstable.value === "yes") {
-					if (latest_unstable && latest_unstable !== "" && messages[1] !== "" && latest_stable.unvalue !== messages[1]) {
+					if (latest_unstable && latest_unstable.value !== "" && messages[1] !== "" && latest_stable.unvalue !== messages[1]) {
 						showNotification("New brunch unstable release available: " + messages[1], "brunch");
 					}
 				}
@@ -66,7 +66,7 @@ function ws_connect() {
 			}
 			if (messages[0] === "latest-chromeos") {
 				if (notifications.value === "yes" && chromeos.value === "yes") {
-					if (latest_chromeos && latest_chromeos !== "" && messages[1] !== "" && latest_chromeos.value !== messages[1]) {
+					if (latest_chromeos && latest_chromeos.value !== "" && messages[1] !== "" && latest_chromeos.value !== messages[1]) {
 						showNotification("New recovery image available: " + messages[1], "chromeos");
 					}
 				}
