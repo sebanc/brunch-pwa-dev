108c0b4341c899227a27f9021f30dc1186750f01
sebanc <sebanc.xda@gmail.com>
2021-05-11 22:04:53 +0200

    Update chromeos.js

--- js/chromeos.js
+++ js/chromeos.js
@@ -58,7 +58,7 @@ window.onload = function () {
 
 	document.getElementById("form3").onsubmit = function () {
 		document.getElementById("log").style.background = "gray";
-		log = "<br>Console log:<br>";
+		log = "<br><center>Console log:</center><br>";
 		document.getElementById("log").innerHTML = log;
 		if (!ws) {
 			return false;
