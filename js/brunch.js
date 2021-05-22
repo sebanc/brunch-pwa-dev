231f1b61745d3f5e2d6fa385c59a40aab7b18873
sebanc <sebanc.xda@gmail.com>
2021-05-22 12:19:22 +0200

    Update brunch.js

--- js/brunch.js
+++ js/brunch.js
@@ -103,7 +103,7 @@ window.onload = function () {
 
 	document.getElementById("form2").onsubmit = function () {
 		document.getElementById("log").style.background = "gray";
-		log = "<center><b>Console log:</b></center><br><br>";
+		log = "<center><b>Console log:</b></center><br>";
 		document.getElementById("log").innerHTML = log;
 		if (!ws) {
 			return false;
